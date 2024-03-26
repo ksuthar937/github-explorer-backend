9dfcb15ebc2c286741ea557d88a4ba5abe5affde
ksuthar937 <ksuthar937@gmail.com>
2024-03-26 12:07:15 +0530

    Small some bug fixes

--- src/Routes/userRoute.js
+++ src/Routes/userRoute.js
@@ -11,16 +11,22 @@ const {
   listUsers,
 } = require("../Controllers/userController");
 
+// GitHub User Data Storage
 router.get("/save-user/:username", saveUser);
 
+// Search Functionality
 router.get("/search-users", searchUserByQuery);
 
+// Mutual Followers as Friends
 router.get("/find-mutual-followers/:username", mutualUsers);
 
+// Soft Delete User Records
 router.delete("/delete-user/:username", deletelUser);
 
+// Update User Details
 router.patch("/update-user/:username", updateUser);
 
+// List Users with Sorting
 router.get("/list-users", listUsers);
 
 module.exports = router;
